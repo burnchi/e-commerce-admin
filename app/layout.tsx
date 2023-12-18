276ef2d8b775832b40c693050d1b5eb3ab65e509
burnchi <1273732363@qq.com>
2023-12-18 20:48:44 +0800

    overview page done

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToastProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,13 +21,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
 
-    <html lang="en">
-      <body className={inter.className}>
-      <ToastProvider/>
-      <ModalProvider/>
-        {children}
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   )
 }
